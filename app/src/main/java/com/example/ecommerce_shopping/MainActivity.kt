cb60f2c5d544d4ded494e6237f7780e016fac290
LiN5BoY <2311549594@qq.com>
2023-05-08 12:48:18 +0800

    0508
    初始创建项目

---
+++ app/src/main/java/com/example/ecommerce_shopping/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ecommerce_shopping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
