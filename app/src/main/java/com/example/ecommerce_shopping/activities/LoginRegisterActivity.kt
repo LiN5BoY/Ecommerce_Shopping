0c92a0ab13dbd4229d6c599cd051e3d6eea70b20
LiN5BoY <2311549594@qq.com>
2023-05-09 09:34:35 +0800

    0509
    import dependences,initialize the fundamental packages

---
+++ app/src/main/java/com/example/ecommerce_shopping/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ecommerce_shopping.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerce_shopping.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ app/src/main/java/com/example/ecommerce_shopping/activities/ShoppingActivity.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce_shopping.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecommerce_shopping.R;
+
+public class ShoppingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping);
+    }
+}
